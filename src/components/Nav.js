505e99974e7925cef6c68e03d6d04d3b4ad18df1
Марис <70753488+KreogenGames@users.noreply.github.com>
2022-05-06 18:46:14 +0300

    Home page, Auth UI and start of Navigation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import colorLogo from '../images/es_logo.png'
 
-const Nav = () => {
+
+const Nav = ({authToken, setShowModal, showModal, setIsSignUp}) => {
+
+    const handleClick = () => {
+        setShowModal(true)
+        setIsSignUp(false)
+    }
+
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <div className="logo-container">
+                <img className="logo" src={colorLogo}/>
+            </div>
+
+            {!authToken && <button
+                className="nav-button"
+                onClick={handleClick}
+                disabled={showModal}
+            >Log In</button>}
+        </nav>
     );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,39 @@
 import React from 'react';
 import Nav from "../components/Nav";
+import {useState} from "react";
+import AuthModal from "../components/AuthModal";
 
 const Home = () => {
+    const [showModal, setShowModal] = useState(false)
+    const [isSignUp, setIsSignUp] = useState(true)
 
     const authToken = false;
 
     const handleClick = () => {
-      console.log('clicked')
+        console.log('clicked')
+        setShowModal(true)
+        setIsSignUp(true)
     }
-    
+
     return (
-        <>
-        <Nav/>
-        <div className="home">
-            <h1>Completed CourseWork is closer then you think</h1>
-            <button className="primary-button" onClick={handleClick}>
-                {authToken ? 'Signout' : 'Create Account'}
-            </button>
+        <div className="overlay">
+            <Nav authToken={authToken}
+                 setShowModal={setShowModal}
+                 showModal={showModal}
+                 setIsSignUp={setIsSignUp}
+            />
+            <div className="home">
+                <h1 className="primary-title">Completed CourseWork is closer then you think</h1>
+                <button className="primary-button" onClick={handleClick}>
+                    {authToken ? 'Signout' : 'Create Account'}
+                </button>
+
+                {showModal && (
+                    <AuthModal setShowModal={setShowModal} isSignUp={isSignUp}/>
+                )}
+
+            </div>
         </div>
-        </>
     );
 };
 
